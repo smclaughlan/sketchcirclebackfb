54d8ea56757becdfe93ac394bb51b67c18f9c705
smclaughlan <sean.s.mclaughlan@gmail.com>
2023-01-18 19:33:27 -0800

    Add firebase-admin

--- app/config.py
+++ app/config.py
@@ -1,6 +1,8 @@
 import os
 
-class Configuration:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+import firebase_admin
+from firebase_admin import credentials
+
+abs_path = os.path.abspath("admin.json")
+cred = credentials.Certificate(abs_path)
+firebase_admin.initialize_app(cred)
