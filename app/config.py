ea2037fd68c46956b23dc36e60bdbd2a6490333d
smclaughlan <sean.s.mclaughlan@gmail.com>
2023-01-23 21:55:44 -0800

    Add firebase db

--- app/config.py
+++ app/config.py
@@ -2,7 +2,9 @@ import os
 
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import firestore
 
 abs_path = os.path.abspath("admin.json")
 cred = credentials.Certificate(abs_path)
-firebase_admin.initialize_app(cred)
+fb = firebase_admin.initialize_app(cred)
+db = firestore.client()
